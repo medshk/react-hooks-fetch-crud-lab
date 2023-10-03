9a513f06a032838cbb0165ecc647734a2bf9faae
med <mohammed.bennaceur98@gmail.com>
2023-10-03 15:52:41 +0100

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,6 +19,28 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    const quizData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex,
+    };
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(quizData),
+    })
+      .then((response) => response.json())
+      .then((newQuiz) => {
+        console.log(newQuiz);
+      });
     console.log(formData);
   }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  function hundleDelete(deletedQuiz) {
+    const updatedQuiz = questions.filter(
+      (question) => question.id !== deletedQuiz.id
+    );
+    setQuestions(updatedQuiz);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }, []);
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDeleteItem={hundleDelete}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
